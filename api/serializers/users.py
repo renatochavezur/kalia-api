095f6ac56c1603524a0e9bc66f718b0753c3cfb7
Renato <renato.chavez@ucsp.edu.pe>
2020-09-19 09:22:39 -0500

    fix serializer user

--- api/serializers/users.py
+++ api/serializers/users.py
@@ -11,4 +11,4 @@ class UserBasicSerializer(serializers.ModelSerializer):
         fields = ['id', 'fullname', 'username', 'phone', 'email', 'birth_day', 'dni', 'occupation',
                   'show_birth_day', 'show_dni', 'show_email', 'show_occupation', 'show_phone',
                   'identification_code']
-        read_only_fields = ['id', 'username']
+        read_only_fields = ['id', 'username', 'identification_code']
